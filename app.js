a33a26fed121278003edf95e9210c1c70451e73b
Chris <shian0222@gmail.com>
2019-12-12 02:00:22 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('test')
+})
+
+app.listen(3000, () => {
+  console.log('app is listening on port 3000')
+})
