c9247a0c25639098e22dd641cae61d544257a929
Chris <shian0222@gmail.com>
2019-12-12 12:18:52 +0800

    add routes and views for register and login page

--- app.js
+++ app.js
@@ -11,8 +11,35 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+//首頁
 app.get('/', (req, res) => {
   res.send('test')
+})
+
+//認證系統路由
+//登錄頁面
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+//登錄檢查動作
+app.post('/users/login', (req, res) => {
+  res.send('login action')
+})
+
+//註冊頁面
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+//註冊動作
+app.post('/users/register', (req, res) => {
+  res.send('register action')
+})
+
+//登出
+app.get('/users/logout', (req, res) => {
+  res.redirect('/')
 })
 
 app.listen(3000, () => {
